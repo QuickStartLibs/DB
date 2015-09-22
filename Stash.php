21afccca7039e7f2c22fed93a6d216b5366342ee
Travis van der Font <travis.font@gmail.com>
2015-09-22 10:21:02 +0200

    -

---
+++ Stash.php
@@ -0,0 +1,18 @@
+<?php
+
+class Stash
+{
+    public static function getQuery($query_name, $directory)
+    {
+        $file = $_SERVER['DOCUMENT_ROOT'].APP_DIR.'queries/'.$directory.'/'.$query_name.'.sql';
+
+        if (file_exists($file))
+        {
+            return trim(file_get_contents($file));
+        }
+        else
+        {
+            die ($file.' doesn\'t exist.');
+        }
+    }
+}
